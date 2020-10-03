b82a0cba2430fcbeb8227a1e2de2835576e04711
masterkram <markbruderer123@gmail.com>
2020-10-03 17:33:55 +0200

    change to bytehub.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'BITHUB',
+  title: 'BYTEHUB',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
